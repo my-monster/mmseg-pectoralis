395c7152ba09cb2d4e4ebbce00b49090ad492938
YuanLiu <123456>
2022-07-10 11:24:45 +0800

    Create some configuration files that satisfy my dataset. Version v1

---
+++ configs/unet/fcn_unet_s5-d16_256x256_40k_hrf_pectoralis.py
@@ -0,0 +1,62 @@
+_base_ = [
+    '../_base_/models/fcn_unet_s5-d16.py', '../_base_/datasets/my_pectoralis.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    backbone=dict(norm_cfg=norm_cfg),
+    decode_head=dict(norm_cfg=norm_cfg),
+    auxiliary_head=dict(norm_cfg=norm_cfg))
+
+img_norm_cfg = dict(
+    mean=[21.601, 21.601, 21.601], std=[43.724, 43.724, 43.724], to_rgb=True)
+
+
+img_scale = (256, 256)
+crop_size = (256, 256)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    # dict(type='Resize', img_scale=img_scale, keep_ratio=True),
+    # dict(type='Resize', img_scale=(1024, 1024)),
+    # dict(type='Resize', img_scale=(1024, 1024), ratio_range=(0.5, 2.0)),
+
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    # dict(type='CLAHE', clip_limit=3),
+    dict(type='AdjustGamma'),
+    dict(type='RandomRotate', prob=0.5, degree=(90,90)),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            # dict(type='Resize', keep_ratio=False),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
+
+
+model = dict(
+    # pretrained='',
+    decode_head=dict(num_classes=2),
+    auxiliary_head=dict(num_classes=2),
+    test_cfg=dict(crop_size=(256, 256), stride=(170, 170)))
+evaluation = dict(metric='mDice')
---
+++ tools/calcuate_mean_and_std.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import os
+
+
+def get_mean_and_std(imgs_path,img_h,img_w):
+    # img_h, img_w = 32, 32
+    means, stdevs = [], []
+    img_list = []
+    imgs_path_list = os.listdir(imgs_path)
+
+    len_ = len(imgs_path_list)
+    i = 0
+    for item in imgs_path_list:
+        img = cv2.imread(os.path.join(imgs_path, item))
+        img = cv2.resize(img, (img_w, img_h))
+        img = img[:, :, :, np.newaxis]
+        img_list.append(img)
+        i += 1
+        print(i, '/', len_)
+
+    imgs = np.concatenate(img_list, axis=3)
+    imgs = imgs.astype(np.float32) / 255.
+
+    for i in range(3):
+        pixels = imgs[:, :, i, :].ravel()  # 拉成一行
+        means.append(np.mean(pixels))
+        stdevs.append(np.std(pixels))
+
+    # BGR --> RGB ， CV读取的需要转换，PIL读取的不用转换
+    means.reverse()
+    stdevs.reverse()
+    means = [i*255 for i in means]
+    stdevs = [i*255 for i in stdevs]
+    
+    print("normMean = {}".format(means))
+    print("normStd = {}".format(stdevs))
+
+
+if __name__ == '__main__':
+    imgs_path = r'D:\SourcetreeSpace\mmseg-pectoralis\data\pectoralis_dataset\train\images'  # 图片目录
+    img_h = 256
+    img_w = 256  # 根据自己数据集适当调整，别太大了，最开始头铁4000、6000速度特别慢
+    get_mean_and_std(imgs_path, img_h, img_w)
